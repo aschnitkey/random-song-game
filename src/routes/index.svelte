82708e6e07c41c9fd57793a4d60d3655c2195e51
Andrew <aschnitkey@gmail.com>
2022-02-07 09:12:19 -0500

    error handling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 	export let band = '';
 	export let album = '';
 	export let song = '';
+	let hasLoadedSong = false;
 
 	const getRandomAlbum = () => {
 		const randomNum = Math.floor(Math.random() * musicData.length + 1);
@@ -20,20 +21,35 @@
 		const url = import.meta.env.VITE_AUDIODB_URL;
 		const apikey = import.meta.env.VITE_AUDIODB_API_KEY;
 		console.log(`URL: ${url}/${apikey}/searchalbum.php?s=${artistName}&a=${albumName}`);
-		const res = await fetch(`${url}/${apikey}/searchalbum.php?s=${artistName}&a=${albumName}`);
-		const data = await res.json();
-		band = data.album[0].strArtist;
-		album = data.album[0].strAlbum;
-		albumId = data.album[0].idAlbum;
-		console.log(`albumId: ${albumId}`);
-		const songRes = await fetch(`${url}/${apikey}/track.php?m=${albumId}`);
-		const songData = await songRes.json();
-		const randomNum = Math.floor(Math.random() * songData.track.length + 1);
-		console.log(randomNum);
-		song = songData.track[randomNum].strTrack;
+		try {
+			const res = await fetch(`${url}/${apikey}/searchalbum.php?s=${artistName}&a=${albumName}`);
+			if (!res.ok) {
+				throw new Error('Response Error!', { cause: res.statusText });
+			}
+			const data = await res.json();
+			band = data.album[0].strArtist;
+			album = data.album[0].strAlbum;
+			albumId = data.album[0].idAlbum;
+			console.log(`albumId: ${albumId}`);
+		} catch (err) {
+			console.log(err);
+		}
+		try {
+			const songRes = await fetch(`${url}/${apikey}/track.php?m=${albumId}`);
+			if (!songRes.ok) {
+				throw new Error('Response Error!', { cause: songRes.statusText });
+			}
+			const songData = await songRes.json();
+			const randomNum = Math.floor(Math.random() * songData.track.length + 1);
+			console.log(randomNum);
+			song = songData.track[randomNum].strTrack;
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
 	const handleClick = () => {
+		hasLoadedSong = true;
 		const { artist, album } = getRandomAlbum();
 		updateSong(artist, album);
 	};
@@ -41,8 +57,14 @@
 
 <div class="container">
 	<h1 class="logo text-3xl font-bold underline uppercase">Today's Random Song</h1>
-	<p>{band}</p>
-	<p>{album}</p>
-	<p>{song}</p>
+	{#if song}
+		<p>{band}</p>
+		<p>{album}</p>
+		<p>{song}</p>
+	{/if}
+	{#if !song && hasLoadedSong}
+		<p>There was a problem loading your track. Please try again!</p>
+		<p>Error Resided with: {band} - {album}</p>
+	{/if}
 	<button on:click={handleClick} class="px-5 py-3 my-5 bg-gray-900 color-white">Get Song</button>
 </div>
